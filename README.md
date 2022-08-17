1b4fb780f26bb1c4a2aee55a74be9180cf42dc31
Chris Siebenmann <cks.git02@cs.toronto.edu>
2022-08-17 10:04:00 -0400

    Make zfs_exporter report more and better information
    This is a collection of improvements that were developed together and
    have not at this time been split into separate individual changes and
    commits. Large changes:
    
    - be able to report stats for pools and disks as well as vdevs.
    - include more IO stats and pool scan stats
    - include some pool information
    - change 'physical' to 'individual' IO stats because the latter is
      more accurate.
    - notice if ioctl.Init() fails.

--- README.md
+++ README.md
@@ -10,3 +10,17 @@ abandoned completely and merged into node_exporter**
 This currently exposes all basic stats (vdev_stats) and most extended stats (vdev_stats_ex) on a
 vdev-level. It doesn't expose per-disk or per-zpool stats, even though these are also available from
 the underlying API.
+
+## Notes for cks-dev branch
+
+This branch is the code that I (Chris Siebenmann) have hacked up to provide more stats on more things. It's not a clean set of changes and it may be rebased periodically; I wanted to publish it because I think it's useful and it's what we're running.
+
+This code will report stats for pools, vdevs, and individual disks depending on the setting of -depth. Stats for disks are normally reported using the short disk name, much like what 'zpool status' reports.
+
+To the best of Chris's knowledge, this can report more or less all extended stats that exist in the development version of OpenZFS as of now, and will report some extended stats as far back as 0.7.5 (Ubuntu 18.04 LTS version). Some statistics have been renamed to make them more accurate, such as the statistics for 'individual' (not 'physical') IO. See
+
+https://utcc.utoronto.ca/~cks/space/blog/solaris/ZFSIndividualVsAggregatedIOs
+
+The vdev names for raidz and draid vdevs don't yet match the libzfs code, as libzfs includes the raidz level and various draid details in the name and this code does not yet do so. In general this code has been used and tested only on mirror vdevs, because that's our case here.
+
+To make full use of ZFS metrics on modern OpenZFS, you'll need both this exporter and a version of (node_exporter)[https://github.com/prometheus/node_exporter] that has been patched to properly handle that current OpenZFS doesn't have the old basic pool iostats any more, so that you get per-dataset stats and other basic ZFS pool information.
--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+// Export ZFS metrics obtained from the kernel as Prometheus metrics.
+// Metrics may be reported for just pools, for pools and top level
+// vdevs, or for pools, vdevs, and disks.
 package main
 
 import (
@@ -6,6 +9,8 @@ import (
 	"log"
 	"math"
 	"net/http"
+	"path/filepath"
+	"strconv"
 
 	"git.dolansoft.org/lorenz/go-zfs/ioctl"
 	"github.com/prometheus/client_golang/prometheus"
@@ -14,6 +19,8 @@ import (
 
 var (
 	listenAddr = flag.String("listen-addr", ":9700", "Address the ZFS exporter should listen on")
+	vdevDepth  = flag.Int("depth", 1, "Depth of the vdev tree to report on. 0 is the pool, 1 is top level vdevs, 2 is devices too")
+	fullPath   = flag.Bool("fullpath", false, "Report the full path of disks")
 )
 
 type stat struct {
@@ -50,7 +57,7 @@ var vdevStats = []stat{
 	{n: "initialize_action_time", d: "initialize time"},
 	{n: "checkpoint_space_bytes", d: "checkpoint space in bytes"},
 	{n: "resilver_deferred", d: "resilver deferred"},
-	{n: "slow_ios", d: "slow I/O operations"},
+	{n: "slow_ios", d: "slow I/O operations (30 seconds or more to complete)"},
 	{n: "trim_errors", d: "trim errors"},
 	{n: "trim_unsupported", d: "doesn't support TRIM"},
 	{n: "trim_processed_bytes", d: "TRIMmed bytes"},
@@ -61,20 +68,61 @@ var vdevStats = []stat{
 	{n: "ashift_configured", d: "configured ashift"},
 	{n: "ashift_logical", d: "logical ashift"},
 	{n: "ashfit_physical", d: "physical ashift"},
+	// Added in 2021 and 2022.
+	{n: "noalloc_status", d: "allocations halted?"},
+	{n: "physical_capacity_bytes", d: "physical capacity"},
+}
+
+// <cks>: this is struct pool_scan_stat.
+// TODO: should we create a derived statistic for the last scan duration,
+// when the scan state is 2? It's not necessarily easy to get it otherwise.
+var scanStats = []stat{
+	{n: "scan_func", d: "Pool scan function: 0 none, 1 scrub, 2 resilver, 3 rebuild (maybe)"},
+	{n: "scan_state", d: "Pool scan state: 0 none, 1 scanning, 2 finished, 3 cancelled"},
+	{n: "scan_start_time_seconds", d: "Pool scan start time"},
+	{n: "scan_end_time_seconds", d: "Pool scan end time"},
+	{n: "scan_to_examine_bytes", d: "Total bytes to scan"},
+	{n: "scan_examined_bytes", d: "Total bytes examined"},
+	{n: "scan_to_process_bytes", d: "Total bytes to process"},
+	{n: "scan_processed_bytes", d: "Total bytes processed"},
+	{n: "scan_errors", d: "Scan errors"},
+	// values not stored on disk.
+	{n: "scan_pass_examined_bytes", d: "Examined bytes per scan pass"},
+	{n: "scan_pass_start_seconds", d: "Start time of a scan pass"},
+	{n: "scan_scrub_pause", d: "Pause time of a scrub pass"},
+	{n: "scan_scrub_pause_time_spent", d: "Cumulative time the scrub spent paused"},
+	{n: "scan_pass_issued_bytes", d: "Issued bytes per scan pass"},
+	{n: "scan_issued_bytes", d: "Total bytes checked by scanner"},
 }
 
 var (
-	extendedStatsLabels = []string{"type", "vdev", "zpool"}
+	extendedStatsLabels = []string{"type", "vdev", "zpool", "path"}
 )
 
+// ZFS IO statistics don't include histograms of physical disk IO.
+// 'Individual' IO histograms (*_ind_*) are for non-aggregated IO.
+// <cks> has renamed zfs_vdev_size_physical to
+// zfs_vdev_size_individual because of this confusion.
 var (
 	activeQueueLength  = prometheus.NewDesc("zfs_vdev_queue_active_length", "Number of ZIOs issued to disk and waiting to finish", extendedStatsLabels, nil)
 	pendingQueueLength = prometheus.NewDesc("zfs_vdev_queue_pending_length", "Number of ZIOs pending to be issued to disk", extendedStatsLabels, nil)
 	queueLatency       = prometheus.NewDesc("zfs_vdev_queue_latency", "Amount of time an IO request spent in the queue", extendedStatsLabels, nil)
 	zioLatencyTotal    = prometheus.NewDesc("zfs_vdev_zio_latency_total", "Total ZIO latency including queuing and disk access time.", extendedStatsLabels, nil)
 	zioLatencyDisk     = prometheus.NewDesc("zfs_vdev_latency_disk", "Amount of time to read/write the disk", extendedStatsLabels, nil)
-	physicalIOSize     = prometheus.NewDesc("zfs_vdev_io_size_physical", "Size of the physical I/O requests issued", extendedStatsLabels, nil)
+	individualIOSize   = prometheus.NewDesc("zfs_vdev_io_size_individual", "Size of the 'individual' non-aggregated I/O requests issued", extendedStatsLabels, nil)
 	aggregatedIOSize   = prometheus.NewDesc("zfs_vdev_io_size_aggregated", "Size of the aggregated I/O requests issued", extendedStatsLabels, nil)
+	poolLoadTime       = prometheus.NewDesc("zfs_pool_load_time_seconds", "The time when the pool was imported (often at system boot)", []string{"zpool", "guid"}, nil)
+	poolErrors         = prometheus.NewDesc("zfs_pool_errors", "ZFS pool error count", []string{"zpool", "guid"}, nil)
+	poolChildren       = prometheus.NewDesc("zfs_pool_vdevs", "ZFS pool top level vdev count", []string{"zpool", "guid"}, nil)
+	vdevChildren       = prometheus.NewDesc("zfs_vdev_children", "Count of children of a vdev", []string{"vdev", "zpool"}, nil)
+
+	// The 'txg' in the root of a pool is apparently the txg of
+	// the most recent configuration change or pool load (eg on
+	// system reboots). It's probably worth reporting this as a
+	// metric simply so that we can pick up configuration changes,
+	// which I believe may include adding and removing devices to
+	// mirror vdevs.
+	poolConfigTxg = prometheus.NewDesc("zfs_pool_config_txg", "ZFS pool configuration load or change txg", []string{"zpool"}, nil)
 )
 
 type extStat struct {
@@ -85,13 +133,16 @@ type extStat struct {
 
 var extStatsMap map[string]extStat
 
+// Extended stats also contain "vdev_slow_ios", but this is the same
+// as a basic vdev statistics field. (Literally; in the kernel code,
+// the basic stat field is copied to the extended stat entry.)
 var extStats = []extStat{
 	{"vdev_agg_scrub_histo", aggregatedIOSize, "scrub"},
 	{"vdev_agg_trim_histo", aggregatedIOSize, "trim"},
 	{"vdev_async_agg_r_histo", aggregatedIOSize, "async_read"},
 	{"vdev_async_agg_w_histo", aggregatedIOSize, "async_write"},
-	{"vdev_async_ind_r_histo", physicalIOSize, "async_read"},
-	{"vdev_async_ind_w_histo", physicalIOSize, "async_write"},
+	{"vdev_async_ind_r_histo", individualIOSize, "async_read"},
+	{"vdev_async_ind_w_histo", individualIOSize, "async_write"},
 	{"vdev_async_r_active_queue", activeQueueLength, "async_read"},
 	{"vdev_async_r_lat_histo", queueLatency, "async_read"},
 	{"vdev_async_r_pend_queue", pendingQueueLength, "async_read"},
@@ -104,13 +155,13 @@ var extStats = []extStat{
 	{"vdev_async_w_pend_queue", pendingQueueLength, "async_write"},
 	{"vdev_disk_r_lat_histo", zioLatencyDisk, "read"},
 	{"vdev_disk_w_lat_histo", zioLatencyDisk, "write"},
-	{"vdev_ind_scrub_histo", physicalIOSize, "scrub"},
-	{"vdev_ind_trim_histo", physicalIOSize, "trim"},
+	{"vdev_ind_scrub_histo", individualIOSize, "scrub"},
+	{"vdev_ind_trim_histo", individualIOSize, "trim"},
 	{"vdev_scrub_histo", queueLatency, "scrub"},
 	{"vdev_sync_agg_r_histo", aggregatedIOSize, "sync_read"},
 	{"vdev_sync_agg_w_histo", aggregatedIOSize, "sync_write"},
-	{"vdev_sync_ind_r_histo", physicalIOSize, "sync_read"},
-	{"vdev_sync_ind_w_histo", physicalIOSize, "sync_write"},
+	{"vdev_sync_ind_r_histo", individualIOSize, "sync_read"},
+	{"vdev_sync_ind_w_histo", individualIOSize, "sync_write"},
 	{"vdev_sync_r_active_queue", activeQueueLength, "sync_read"},
 	{"vdev_sync_r_lat_histo", queueLatency, "sync_read"},
 	{"vdev_sync_r_pend_queue", pendingQueueLength, "sync_read"},
@@ -120,6 +171,13 @@ var extStats = []extStat{
 	{"vdev_tot_r_lat_histo", zioLatencyTotal, "read"},
 	{"vdev_tot_w_lat_histo", zioLatencyTotal, "write"},
 	{"vdev_trim_histo", queueLatency, "trim"},
+
+	// These are in the ZoL development version as of 2022-07.
+	{"vdev_rebuild_active_queue", activeQueueLength, "rebuild"},
+	{"vdev_rebuild_pend_queue", pendingQueueLength, "rebuild"},
+	{"vdev_ind_rebuild_histo", individualIOSize, "rebuild"},
+	{"vdev_agg_rebuild_histo", aggregatedIOSize, "rebuild"},
+	{"vdev_rebuild_histo", queueLatency, "rebuild"},
 }
 
 func init() {
@@ -128,10 +186,13 @@ func init() {
 			continue
 		}
 		if len(s.variants) == 0 {
-			vdevStats[i].desc = prometheus.NewDesc("zfs_vdev_"+s.n, "ZFS VDev "+s.d, []string{"vdev", "zpool"}, nil)
+			vdevStats[i].desc = prometheus.NewDesc("zfs_vdev_"+s.n, "ZFS VDev "+s.d, []string{"vdev", "zpool", "path"}, nil)
 		} else {
-			vdevStats[i].desc = prometheus.NewDesc("zfs_vdev_"+s.n, "ZFS VDev "+s.d, []string{"vdev", "zpool", s.dimension}, nil)
+			vdevStats[i].desc = prometheus.NewDesc("zfs_vdev_"+s.n, "ZFS VDev "+s.d, []string{"vdev", "zpool", "path", s.dimension}, nil)
 		}
+	}
+	for i, s := range scanStats {
+		scanStats[i].desc = prometheus.NewDesc("zfs_pool_"+s.n, "ZFS Pool Scan "+s.d, []string{"zpool"}, nil)
 	}
 	extStatsMap = make(map[string]extStat)
 	for _, v := range extStats {
@@ -148,13 +209,142 @@ func (c *zfsCollector) Describe(ch chan<- *prometheus.Desc) {
 		}
 		ch <- s.desc
 	}
+	for _, s := range scanStats {
+		ch <- s.desc
+	}
 	ch <- activeQueueLength
 	ch <- pendingQueueLength
 	ch <- queueLatency
 	ch <- zioLatencyTotal
 	ch <- zioLatencyDisk
-	ch <- physicalIOSize
+	ch <- individualIOSize
 	ch <- aggregatedIOSize
+	ch <- poolLoadTime
+	ch <- poolErrors
+	ch <- poolChildren
+	ch <- vdevChildren
+	ch <- poolConfigTxg
+}
+
+// vdevName generates the name for the vdev= label.
+// This, *plus* the path of the vdev, must be unique.
+// A normal top-level vdev has a name of '<type>-<id>'.
+// A top-level disk has a name of 'disk-<id>'.
+// A nested vdev, which you can get while resilvering,
+// has a name of '<parent>/<type>-<id>', eg
+// "mirror-1/...".
+// A disk nested inside a parent vdev has the vdev name
+// of the parent; it will be distinguished based on the
+// path= label.
+//
+// In OpenZFS, this is done by the libzfs function
+// zpool_vdev_name(), which has somewhat more complex
+// rules that are intended to provide more human friendly
+// names. We may at some time immitate its naming, so that
+// we generate names like 'raidz2-0' instead of 'raidz-0'.
+// (dRAID is even more complicated.)
+func vdevName(parent string, vdev map[string]interface{}) string {
+	typ := vdev["type"].(string)
+	if typ == "disk" && parent != "" {
+		return parent
+	}
+	if parent != "" {
+		parent = parent + "/"
+	}
+	// TODO: This doesn't always seem to match what zpool shows
+	return fmt.Sprintf("%s%s-%d", parent, vdev["type"], vdev["id"])
+}
+
+// reportVdevStats reports on 'vdev' stats, both basic and extended.
+// Vdevs, the pool, and individual devices all have vdev stats, but
+// not all stats are valid for all types. We report anything that
+// actually exists in the data. Stats that are inapplicable for a type
+// are generally 0, and Prometheus is quite efficient at storing constant
+// data.
+//
+// We report a non-blank path label for anything that has
+// one. Normally this is physical disks. Otherwise, the vdev type is
+// implicit in its name, eg "mirror-0" is a mirror. Physical disks
+// have the vdev name of their parent vdev.
+func reportVdevStats(poolName, vdevName string, vdev map[string]interface{}, ch chan<- prometheus.Metric) {
+	rawStats := vdev["vdev_stats"].([]uint64)
+	path, ok := vdev["path"].(string)
+	if !ok {
+		path = ""
+	} else if !*fullPath {
+		path = filepath.Base(path)
+	}
+
+	// vdevStats entries with variants actually cover (and
+	// consume) multiple raw stats, forcing us to avoid
+	// the simple iteration approach.
+	i := 0
+	for _, s := range vdevStats {
+		if i >= len(rawStats) {
+			break
+		}
+		if s.n == "" {
+			i++
+			continue
+		}
+		if len(s.variants) == 0 {
+			ch <- prometheus.MustNewConstMetric(s.desc, prometheus.UntypedValue, float64(rawStats[i]), vdevName, poolName, path)
+			i++
+		} else {
+			for _, v := range s.variants {
+				ch <- prometheus.MustNewConstMetric(s.desc, prometheus.UntypedValue, float64(rawStats[i]), vdevName, poolName, path, v)
+				i++
+			}
+		}
+	}
+	extended_stats := vdev["vdev_stats_ex"].(map[string]interface{})
+	for name, val := range extended_stats {
+		statMeta := extStatsMap[name]
+		if statMeta.name == "" {
+			continue
+		}
+		if scalar, ok := val.(uint64); ok {
+			ch <- prometheus.MustNewConstMetric(statMeta.desc, prometheus.GaugeValue, float64(scalar), statMeta.label, vdevName, poolName, path)
+		} else if histo, ok := val.([]uint64); ok {
+			buckets := make(map[float64]uint64)
+			var count uint64
+			var acc float64
+			var divisor float64 = 1.0
+			if len(histo) == 37 {
+				divisor = 1_000_000_000 // 1 ns in s
+			}
+			for i, v := range histo {
+				count += v
+				buckets[math.Exp2(float64(i))/divisor] = count
+				midpoint := (1 << i) + ((1 << i) / 2)
+				// This mimics the calculation that
+				// 'zpool iostat' does. We can't do
+				// any better because the raw sum
+				// information isn't exported by ZFS.
+				acc += float64(v) * float64(midpoint)
+			}
+			// <cks>: the upstream version punts on an
+			// accumulated value (and calls the count
+			// 'acc', just to fool you).
+			ch <- prometheus.MustNewConstHistogram(statMeta.desc, count, acc/divisor, buckets, statMeta.label, vdevName, poolName, path)
+		} else {
+			log.Fatalf("invalid type encountered: %T", val)
+		}
+	}
+}
+
+func descendVdev(poolName, parent string, vdev map[string]interface{}, ch chan<- prometheus.Metric) {
+	chld := vdev["children"]
+	if chld == nil {
+		return
+	}
+	kids := chld.([]map[string]interface{})
+	ch <- prometheus.MustNewConstMetric(vdevChildren, prometheus.GaugeValue, float64(len(kids)), parent, poolName)
+	for _, v := range kids {
+		vdn := vdevName(parent, v)
+		reportVdevStats(poolName, vdn, v, ch)
+		descendVdev(poolName, vdn, v, ch)
+	}
 }
 
 func (c *zfsCollector) Collect(ch chan<- prometheus.Metric) {
@@ -167,61 +357,50 @@ func (c *zfsCollector) Collect(ch chan<- prometheus.Metric) {
 		if err != nil {
 			panic(err)
 		}
+
+		// TODO: should the number of children be reported as
+		// a separate metric? Should we report the import
+		// time?
+		// children := stats["vdev_children"].(uint64)
+		guid := strconv.FormatUint(stats["pool_guid"].(uint64), 10)
+		ltimes := stats["initial_load_time"].([]uint64)
+		ch <- prometheus.MustNewConstMetric(poolLoadTime, prometheus.GaugeValue, float64(ltimes[0]), poolName, guid)
+		ch <- prometheus.MustNewConstMetric(poolErrors, prometheus.GaugeValue, float64(stats["error_count"].(uint64)), poolName, guid)
+		ch <- prometheus.MustNewConstMetric(poolChildren, prometheus.GaugeValue, float64(stats["vdev_children"].(uint64)), poolName, guid)
+		ch <- prometheus.MustNewConstMetric(poolConfigTxg, prometheus.GaugeValue, float64(stats["txg"].(uint64)), poolName)
+
 		vdevTree := stats["vdev_tree"].(map[string]interface{})
 		vdevs := vdevTree["children"].([]map[string]interface{})
-		for _, vdev := range vdevs {
-			// TODO: This doesn't always seem to match what zpool shows
-			vdevName := fmt.Sprintf("%s-%d", vdev["type"], vdev["id"])
-			rawStats := vdev["vdev_stats"].([]uint64)
-			i := 0
-			for _, s := range vdevStats {
+		reportVdevStats(poolName, "root", vdevTree, ch)
+		if *vdevDepth > 0 {
+			for _, vdev := range vdevs {
+				vdn := vdevName("", vdev)
+				reportVdevStats(poolName, vdn, vdev, ch)
+				if *vdevDepth > 1 {
+					descendVdev(poolName, vdn, vdev, ch)
+				}
+			}
+		}
+
+		// Report pool scan stats.
+		if vdevTree["scan_stats"] != nil {
+			rawStats := vdevTree["scan_stats"].([]uint64)
+			for i, s := range scanStats {
 				if i >= len(rawStats) {
 					break
 				}
-				if s.n == "" {
-					i++
-					continue
-				}
-				if len(s.variants) == 0 {
-					ch <- prometheus.MustNewConstMetric(s.desc, prometheus.UntypedValue, float64(rawStats[i]), vdevName, poolName)
-					i++
-				} else {
-					for _, v := range s.variants {
-						ch <- prometheus.MustNewConstMetric(s.desc, prometheus.UntypedValue, float64(rawStats[i]), vdevName, poolName, v)
-						i++
-					}
-				}
-			}
-			extended_stats := vdev["vdev_stats_ex"].(map[string]interface{})
-			for name, val := range extended_stats {
-				statMeta := extStatsMap[name]
-				if statMeta.name == "" {
-					continue
-				}
-				if scalar, ok := val.(uint64); ok {
-					ch <- prometheus.MustNewConstMetric(statMeta.desc, prometheus.GaugeValue, float64(scalar), statMeta.label, vdevName, poolName)
-				} else if histo, ok := val.([]uint64); ok {
-					buckets := make(map[float64]uint64)
-					var acc uint64
-					var divisor float64 = 1.0
-					if len(histo) == 37 {
-						divisor = 1_000_000_000 // 1 ns in s
-					}
-					for i, v := range histo {
-						acc += v
-						buckets[math.Exp2(float64(i))/divisor] = acc
-					}
-					ch <- prometheus.MustNewConstHistogram(statMeta.desc, acc, 0.0, buckets, statMeta.label, vdevName, poolName)
-				} else {
-					log.Fatalf("invalid type encountered: %T", val)
-				}
+				// We know for sure that these are all gauges.
+				ch <- prometheus.MustNewConstMetric(s.desc, prometheus.GaugeValue, float64(rawStats[i]), poolName)
 			}
 		}
 	}
 }
 
 func main() {
-	ioctl.Init("")
+	err := ioctl.Init("")
+	if err != nil {
+		log.Fatalf("ioctl.Init failed: %v", err)
+	}
 
 	c := zfsCollector{}
 	prometheus.MustRegister(&c)
